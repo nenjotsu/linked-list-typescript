517b334ecefd291a7a7e9f36228c1ea443b3dec7
nenjotsu <nenjotsu@protonmail.com>
2022-05-27 18:10:59 +0800

    add vite vitest

--- LICENSE.md
+++ LICENSE.md
@@ -1,6 +1,6 @@
 MIT License
 
-Copyright (c) 2018 Michael Sutherland
+Copyright (c) 2018 Nenjo Tsu
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
---
+++ vite.config.ts
@@ -0,0 +1,30 @@
+/// <reference types="vitest" />
+// Configure Vitest (https://vitest.dev/config/)
+
+import { resolve } from 'path'
+import dts from 'vite-plugin-dts'
+import { defineConfig } from 'vite';
+import { VitePluginNode } from 'vite-plugin-node';
+
+export default defineConfig({
+  server: {
+    port: 3699,
+  },  
+  // build: {
+  //   minify: "esbuild"  
+  // },
+  plugins: [
+    ...VitePluginNode({
+      adapter: 'express',
+      appPath: './src/index.ts',
+      exportName: "FixedLinkedList"
+    }),
+    dts()
+  ],
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/index.ts'),
+      formats: ['es']
+    }
+  }
+});  
